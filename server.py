99a50ff3ec25898c15fdaaf7c543b987e892575f
YutoOtake <yuto.programming@gmail.com>
2025-04-30 21:52:31 +0900

    feat: Add data get endpoint

--- server.py
+++ server.py
@@ -1,18 +1,51 @@
 import asyncio
 import tornado
+import json
+
+# 取得データ格納領域
+posts = {}
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+    async def get(self):
+        # 複数データ取得
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        try:
+            response = await http_client.fetch('https://jsonplaceholder.typicode.com/posts/')
+            if response.error:
+                self.set_status(500)
+                self.write({"error": str(response.error)})
+            else:
+                global posts 
+                posts = json.loads(response.body.decode('utf-8'))
+                self.write({'ok': response.code})
+        finally:
+            http_client.close()
     
-class Sample(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Sample")
+class Data(tornado.web.RequestHandler):
+    def get(self, post_id):
+        # 1データ取得
+        try:
+            post_id = int(post_id)
+            found_post = None
+
+            for post in posts:
+                if post['id'] == post_id:
+                    found_post = post
+                    break
+            if found_post:
+                self.write(found_post)
+        except ValueError:
+            self.set_status(400)
+            self.write({"error": "Invalid Value"})
+        except Exception as e:
+            self.write({'error': str(e)})
+        
+
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/sample", Sample)
+        (r"/data/([0-9]+)", Data)
     ])
 
 async def main():
