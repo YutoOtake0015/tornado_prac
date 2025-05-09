d7524934813493f063d8e8dbdc47e29c8876c7b6
YutoOtake <yuto.programming@gmail.com>
2025-05-10 00:19:42 +0900

    feat: Add delete function

--- server.py
+++ server.py
@@ -34,27 +34,33 @@ class ItemHandler(tornado.web.RequestHandler):
     def post(self, item_id):
         try:
             item_id = int(item_id)
-            title = self.get_argument("title")
-            content = self.get_argument("content")
+            if "/delete" in self.request.path:
+                global items
+                items = [i for i in items if i['id'] != item_id]
+            else:
+                item_id = int(item_id)
+                title = self.get_argument("title")
+                content = self.get_argument("content")
 
-            for i in items:
-                if i["id"] == item_id:
-                    item = i
-                    break
-            
-            item["title"] = title
-            item["content"] = content
+                for i in items:
+                    if i["id"] == item_id:
+                        item = i
+                        break
+
+                item["title"] = title
+                item["content"] = content
 
             self.set_status(200)
             self.redirect("/") 
         except Exception as e:
             self.set_status(400)
             self.write(json.dumps({"error": str(e)}))
-    
+
 def make_app():
     return tornado.web.Application([
         (r"/", Mainhandler),
         (r"/items/([0-9]+)", ItemHandler),
+        (r"/items/([0-9]+)/delete", ItemHandler),
     ],
         template_path=os.path.join(os.getcwd(),"templates"),
         static_path=os.path.join(os.getcwd(),"static"),
